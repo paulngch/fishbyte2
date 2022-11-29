6c795968df8031d22dbafe624abf2952b6fa00b4
Your Name <you@example.com>
2022-11-29 14:12:17 +0800

    Fixed homepage date-picker

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
       <div className="pageTabs mx-auto pt-10 relative">
         <div className=" max-w-[300px] text-xl">{randomQuote}</div>
       </div>
-      <div><DateDropdown/></div>
     </>
   );
 };
